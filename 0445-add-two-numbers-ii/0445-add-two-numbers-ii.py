7e262bc1149121e6ff87a20e9d97c91bca0e675c
akshatlakhera603-rgb <akshatlakhera603@gmail.com>
2026-09-11 23:25:46 +0530

    Time: 7 ms (37.14%), Space: 19.3 MB (46.93%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -1,58 +1,32 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        prev = None
-        curr = l1
+    def addTwoNumbers(self, l1, l2):
 
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        l1=prev
-        prev = None
-        curr = l2
+        stack1 = []
+        stack2 = []
 
-        while curr:
-            next = curr.next
-            curr.next = prev
-            prev = curr
-            curr = next
-        l2=prev
-        dummy=ListNode(0)
-        l3=dummy
-        temp=l1
-        temp1=l2
-        carry=0
-        while temp or temp1 or carry:
-            val1=temp.val if temp else 0
-            val2=temp1.val if temp1 else 0
-            total=val1+val2+carry
-            carry=(total//10)
-            total=(total%10)
-            l3.next=ListNode(total)
-            l3=l3.next
+        while l1:
+            stack1.append(l1.val)
+            l1 = l1.next
 
-            if temp:
-                temp=temp.next
-            if temp1:
-                temp1=temp1.next
-        prev=None 
-        curr=dummy.next
-        while curr:
-            next=curr.next
-            curr.next=prev
-            prev=curr
-            curr=next
-        return prev
+        while l2:
+            stack2.append(l2.val)
+            l2 = l2.next
 
-        
+        carry = 0
+        head = None
 
-        
+        while stack1 or stack2 or carry:
 
-        
-        
+            a = stack1.pop() if stack1 else 0
+            b = stack2.pop() if stack2 else 0
+
+            total = a + b + carry
+
+            digit = total % 10
+            carry = total // 10
+
+            new = ListNode(digit)
+            new.next = head
+            head = new
+
+        return head
