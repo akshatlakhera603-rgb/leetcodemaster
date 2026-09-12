8c5ef8fd67395bebadd4867832cfde30d5bfd5e0
akshatlakhera603-rgb <akshatlakhera603@gmail.com>
2026-09-12 23:39:15 +0530

    Time: 0 ms (100%), Space: 19.5 MB (28.79%) - LeetHub

---
+++ 0078-subsets/0078-subsets.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans=[]
+        def solve(i,curr):
+            if i==len(nums):
+                ans.append(curr.copy())
+                return
+            #include#
+            curr.append(nums[i])
+            solve(i+1,curr)
+            curr.pop()
+            #exclude#
+            solve(i+1,curr)
+
+        solve(0,[])
+        return ans
+
+        
