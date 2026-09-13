9397469eb45eca5bc1b4400522f1bfda423b6101
akshatlakhera603-rgb <akshatlakhera603@gmail.com>
2026-09-13 23:20:56 +0530

    Time: 0 ms (100%), Space: 19.2 MB (64.43%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,9 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+        if word.isupper() or word.islower() or word.istitle():
+            return True
+        else:
+            return False
+        
+        
+        
