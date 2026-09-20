c97a4673d003cfd3bdc49787ee40799999eab20b
akshatlakhera603-rgb <akshatlakhera603@gmail.com>
2026-09-20 22:04:34 +0530

    Time: 3 ms (96.54%), Space: 19.4 MB (10.08%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,18 +1,18 @@
+from collections import deque
 class Solution:
     def reverse(self, x: int) -> int:
         sign=1
         if x<0:
             sign=-1
             x=-x
-        stack=[]
+        stack=deque([])
         while x!=0:
             rem=x%10
             stack.append(rem)
             x//=10
         ans=0
         while stack:
-            ans=ans*10+stack[0]
-            stack.pop(0)
+            ans=ans*10+stack.popleft()
         ans*=sign
         if ans < -(2**31) or ans > 2**31 - 1:
             return 0
