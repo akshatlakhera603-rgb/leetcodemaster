39c24cad308a1fa66c69bd13c29749e653c634e3
akshatlakhera603-rgb <akshatlakhera603@gmail.com>
2026-09-20 21:39:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (70.86%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lastStoneWeight(self, stones: list[int]) -> int:
+        while len(stones) > 1:
+            stones.sort()
+            a = stones.pop()
+            b = stones.pop()
+            if a != b:
+                stones.append(a - b)
+        return stones[0] if stones else 0
+        
